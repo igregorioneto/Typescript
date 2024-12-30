9e21b3fff76301b7456f2ad97e9f266879dfc463
igregorioneto <igregoriodoneto@outlook.com>
2024-12-30 08:56:44 -0300

    feat: Initial structure and Data Types

---
+++ README.md
@@ -0,0 +1,2 @@
+# Typescript
+Studies related to Typescript
---
+++ basics/data-types.ts
@@ -0,0 +1,26 @@
+/**
+ * Number: Usado para representar Float e Números
+ * Boolean: Representa True ou False
+ * String: Usado para representar uma sequência de caracteres
+ * Void: Geralmente usado para retorno de funções
+ * Null: É usado quando um objeto não possui um valor
+ * Undefined: É usado quando a variável não é inicializada
+ * Any: Qualquer valor poderá ser atribuída a esta variável
+ */
+
+let a:null = null;
+let b:number = 123;
+let c:number = 123.123;
+let d:string = 'Geeks';
+let e:undefined = undefined;
+let f:boolean = true;
+let g:number = 0b111001;
+let h:number = 0o436;
+let i:number = 0xadf0d;
+
+let j: any = null; 
+let k: any = 123;
+let l: any = 123.456;
+let m: any = 'Geeks';
+let n: any = undefined; 
+let o: any = true;
